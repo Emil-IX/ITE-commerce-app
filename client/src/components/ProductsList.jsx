a7c50e0c520106ed2dc527cfd8308a91c6e14d9b
Emil-Spi <xiriusix@gmail.com>
2025-10-22 18:16:38 -0400

    v2.6.5 fixes

--- client/src/components/ProductsList.jsx
+++ client/src/components/ProductsList.jsx
@@ -3,6 +3,14 @@ import { useShop } from '../context/ShopContextProvider'
 export default function ProductsList() {
     const { data , filterProducts } = useShop()
    
+  const cutDescription = (text, lenght) => {
+    if(text && text.length > lenght){
+      return text.substring(0, lenght) + '...';
+    } else {
+      return text
+    }
+  }
+
   return (
     <div className="container-xl">
             {filterProducts &&  filterProducts.map(product =>(
@@ -11,7 +19,7 @@ export default function ProductsList() {
                     <img src={product.image_url} alt="" />
                     <p className="price">${product.price}</p>
                     <p><strong>Stock: </strong>{product.stock}</p>
-                    <p><strong>Description: </strong>{product.description}</p>
+                    <p><strong>Description: </strong>{cutDescription(product.description, 60)}</p>
                 </div>
             ))}
         </div>
